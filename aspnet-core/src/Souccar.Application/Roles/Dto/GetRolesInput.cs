f38fb5cf3d01cf3a79867a69bc35f3b01a20147e
AmerSouccar <amersouccar@hotmail.com>
2023-10-19 14:01:37 +0200

    T

--- aspnet-core/src/Souccar.Application/Roles/Dto/GetRolesInput.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Souccar.Roles.Dto
-{
-    public class GetRolesInput
-    {
-        public string Permission { get; set; }
-    }
-}
--- aspnet-core/src/Souccar.Core/Configuration/AppSettingNames.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Souccar.Configuration
-{
-    public static class AppSettingNames
-    {
-        public const string UiTheme = "App.UiTheme";
-    }
-}
